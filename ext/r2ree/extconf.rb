4cdf7b76c00a4e58859c366f31a007b6c662f28b
namusyaka <namusyaka@gmail.com>
2016-10-09 04:38:07 +0900

    add r2ree as a submodule

--- ext/r2ree/extconf.rb
+++ ext/r2ree/extconf.rb
@@ -1,13 +1,13 @@
 require 'mkmf'
 
-$CXXFLAGS += " -std=c++11 -Wall -Wextra -I../../r2ree/include"
-
 CWD = File.expand_path(__dir__)
-R2REE_DIR = File.join(CWD, '..', '..', 'r2ree')
+R2REE_DIR = File.join(CWD, '..', '..', 'vendor', 'r2ree')
 
 Dir.chdir(R2REE_DIR) do
-  FileUtils.cp File.join('include', 'r2ree.hh'), File.join('..', 'ext', 'r2ree', 'libr2ree.hh')
-  FileUtils.cp File.join('src',     'r2ree.cc'), File.join('..', 'ext', 'r2ree', 'libr2ree.cc')
+  FileUtils.cp File.join('include', 'r2ree.hh'), File.join('..', '..', 'ext', 'r2ree', 'libr2ree.hh')
+  FileUtils.cp File.join('src',     'r2ree.cc'), File.join('..', '..', 'ext', 'r2ree', 'libr2ree.cc')
 end
+
+$CXXFLAGS += " -std=c++11 -Wall -Wextra -I#{R2REE_DIR}/include"
 
 create_makefile('r2ree/r2ree')
