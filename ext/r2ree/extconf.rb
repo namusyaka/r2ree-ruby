7678246f3ce08ffd44c277d17b969b54af75b688
namusyaka <namusyaka@gmail.com>
2016-10-10 01:22:45 +0900

    try to fix build failure

--- ext/r2ree/extconf.rb
+++ ext/r2ree/extconf.rb
@@ -8,6 +8,10 @@ Dir.chdir(R2REE_DIR) do
   FileUtils.cp File.join('src',     'r2ree.cc'), File.join('..', '..', 'ext', 'r2ree', 'libr2ree.cc')
 end
 
-$CXXFLAGS += " -std=c++11 -Wall -Wextra -I#{R2REE_DIR}/include"
+$CXXFLAGS +=
+  case cc_command
+  when /clang\+\+/ then " -std=c++11 -Wall -Wextra -I#{R2REE_DIR}/include"
+  when /g++/       then " -std=c++0x -Wall -Wextra -I#{R2REE_DIR}/include"
+  end
 
 create_makefile('r2ree/r2ree')
